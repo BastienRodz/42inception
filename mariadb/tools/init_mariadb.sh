c8ac6170635443ec968793f05e5bc9e865652bb3
Bastien Rodriguez <barodrig@MacBook-Pro-de-Bastien.local>
2021-06-15 14:52:18 +0200

    New commit

--- mariadb/tools/init_mariadb.sh
+++ mariadb/tools/init_mariadb.sh
@@ -6,16 +6,17 @@
 #    By: barodrig <barodrig@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2021/02/10 12:07:34 by barodrig          #+#    #+#              #
-#    Updated: 2021/06/11 11:28:28 by barodrig         ###   ########.fr        #
+#    Updated: 2021/06/15 14:50:56 by barodrig         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 service mysql start
 
-#Configuring wordpress database
-echo "CREATE DATABASE wordpress;" | mysql -u root --skip-password
-echo "GRANT ALL PRIVILEGES ON wordpress.* TO 'root'@'localhost' WITH GRANT OPTION;" | mysql -u root --skip-password
-echo "update mysql.user set plugin='mysql_native_password' where user='root';" | mysql -u root --skip-password
-echo "FLUSH PRIVILEGES;" | mysql -u root --skip-password
+mysql -u root -e "CREATE DATABASE ${MARIA_DATABASE}"
+mysql -u root -e "CREATE USER ${MARIA_LOGIN}@'42inception' IDENTIFIED BY ${MARIA_PASS}"
+mysql -u root -e "GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE} TO ${MARIA_LOGIN}@'42inception' IDENTIFIED BY ${MARIA_PASS}"
+mysql -u root -e "GRANT ALL PRIVILEGES ON *.* TO ${MARIA_LOGIN}@'42inception' IDENTIFIED BY ${MARIA_PASS}"
+mysql -u root -e "FLUSH PRIVILEGES"
+mysql -u root -p ${MARIA_PASS} ${MARIA_DATABASE} #< /tmp/wp_database.sql
 
-bash
+exec mysqld_safe --datadir="var/lib/mysql"
--- wordpress/init_wordpress.sh
+++ wordpress/init_wordpress.sh
@@ -6,35 +6,8 @@
 #    By: barodrig <barodrig@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2021/02/10 12:07:34 by barodrig          #+#    #+#              #
-#    Updated: 2021/06/11 13:16:12 by barodrig         ###   ########.fr        #
+#    Updated: 2021/06/15 13:14:20 by barodrig         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
-#Configuring wordpress database
-echo "CREATE DATABASE wordpress;" | mysql -u root --skip-password
-echo "GRANT ALL PRIVILEGES ON wordpress.* TO 'root'@'localhost' WITH GRANT OPTION;" | mysql -u root --skip-password
-echo "update mysql.user set plugin='mysql_native_password' where user='root';" | mysql -u root --skip-password
-echo "FLUSH PRIVILEGES;" | mysql -u root --skip-password
-
-#Installing PhpMyAdmin
-cd /var/www/html/
-wget https://files.phpmyadmin.net/phpMyAdmin/5.0.1/phpMyAdmin-5.0.1-english.tar.gz
-tar -xvf phpMyAdmin-5.0.1-english.tar.gz && rm -rf phpMyAdmin-5.0.1-english.tar.gz
-mv phpMyAdmin-5.0.1-english/  phpmyadmin/
-mv /tmp/config.inc.php /var/www/html/phpmyadmin/
-rm /var/html/phpmyadmin/config.sample.inc.php
-cd ../../../../
-
-#Installing Wordpress
-cd /tmp/
-wget https://wordpress.org/latest.tar.gz
-tar -xvf latest.tar.gz && rm -rf latest.tar.gz
-mv wordpress/* /var/www/wordpress
-mv /tmp/wp-config.php /var/www/wordpress/
-cd ../
-rm /var/www/wordpress/wp-config-sample.php
-mkdir /var/www/wordpress/phpmyadmin/tmp
-
 php7.3-fpm --nodaemonize
-
-bash
